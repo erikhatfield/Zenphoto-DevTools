aeb99543a36c627da47ef3c084d648627c60ba34
2xd <2xd>
2014-07-12 19:46:58 -0700

    First commit for the Zenphoto DevTools's theme demo (starter kit) designated as the most basic files.
    HTML5 boilerplate, including normalize.css is built into the included head and foot for every theme page.
    
    Plans for the slideshow.php are in the works.

--- demo_plugin-and-theme/demo-theme-most-basic/theme_description.php
+++ demo_plugin-and-theme/demo-theme-most-basic/theme_description.php
@@ -4,8 +4,8 @@
  * Change the values to match your theme
  */
 $theme_description['name'] = '3rikSimplyZen'; // This line is required!
-$theme_description['author'] = '@3rikhatfield';
+$theme_description['author'] = '@3rikhatfield & Zenphoto-DevTools';
 $theme_description['version'] = '1.0';
-$theme_description['date'] = '07/10/2014';
-$theme_description['desc'] = gettext("Originally designated as the minimal-set-of-files theme. View the demo at <a href=\"http://3rik.me/\">3rik.me</a>.");
+$theme_description['date'] = '07/12/2014';
+$theme_description['desc'] = gettext("Originally designated as the minimal-set-of-files theme. View the demo at <a href=\"http://3rik.me/\">3rik.me</a> or <a href=\"https://github.com/erikhatfield/Zenphoto-DevTools/tree/master/demo_plugin-and-theme/demo-theme-most-basic\" target="_blank">view github source</a>.");
 ?>
